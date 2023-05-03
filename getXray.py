8b31bed9958bd8baa9631f0b429a14f58045d6ae
00757033 <58459235+00757033@users.noreply.github.com>
2023-05-03 21:15:04 +0800

    Xray

--- getXray.py
+++ getXray.py
@@ -29,8 +29,8 @@ with open(json_test_path, 'r') as f:
 
 a= list()
 b= list()
-question_closed = list()
-question_open = list()
+question_closed = dict()
+question_open = dict()
 closed = open("XrayClosedAnswer.csv", 'w', encoding='UTF8', newline='')
 write_closed =csv.writer(closed)
 write_closed.writerow(['image_name', 'question','answer'])
@@ -53,14 +53,21 @@ for data in trainset.iloc() :
         if data['image_organ']== 'CHEST':
             trainwriter.writerow([data['image_name'], data['question'], data['answer']])
             if data['answer_type'] == 'CLOSED':
-                write_closed.writerow([data['question'], data['answer']])
-                question_closed.append([data['question'], data['answer']])
+                write_closed.writerow([data['image_name'],data['question'], data['answer']])
+                if data['image_name'] not in question_closed :
+                    question_closed[data['image_name']] = list()
+
+                question_closed[data['image_name']].append([data['question'], data['answer']])
             else:
-                write_opened.writerow([data['question'], data['answer']])
-                question_open.append([data['question'], data['answer']])
+                write_opened.writerow([data['image_name'],data['question'], data['answer']])
+                if data['image_name'] not in question_open :
+                    question_open[data['image_name']] = list()
+                question_open[data['image_name']].append([data['question'], data['answer']])
+                
             a.append(data)
     
 print(len(a))
+
 
 traincsvfile.close()
 
@@ -69,18 +76,50 @@ for data in testset.iloc():
         if data['image_organ']== 'CHEST':
             testwriter.writerow([data['image_name'], data['question'], data['answer']])
             if data['answer_type'] == 'CLOSED':
-                write_closed.writerow([data['question'], data['answer']])
-                question_closed.append([data['question'], data['answer']])
+                write_closed.writerow([data['image_name'],data['question'], data['answer']])
+                if data['image_name'] not in question_closed :
+                    question_closed[data['image_name']] = list()
+                question_closed[data['image_name']].append([data['question'], data['answer']])
             else:
-                write_opened.writerow([data['question'], data['answer']])
-                question_open.append([data['question'], data['answer']])
+                write_opened.writerow([data['image_name'],data['question'], data['answer']])
+                if data['image_name'] not in question_open :
+                    question_open[data['image_name']] = list()
+                question_open[data['image_name']].append([data['question'], data['answer']])
             b.append(data)
+
     
 print(len(b))
 
 testcsvfile.close()
 closed.close()
 opened.close()
+
+write_image_closed = open("QuestionClosed.txt", 'w', encoding='UTF8', newline='')
+write_image_opened = open("QuestionOpened.txt", 'w', encoding='UTF8', newline='')
+
+for key in question_closed:
+    write_image_closed.write(key + " "+ str(len(question_closed[key]))+ "\n")
+    print(key, len(question_closed[key]))
+
+    for item in question_closed[key]:
+        write_image_closed.write(str(item) + "\n")
+    write_image_closed.write("\n")
+    write_image_closed.write("============================================================\n")
+
+
+
+for key in question_open:
+    write_image_opened.write(key + " "+ str(len(question_open[key]))+ "\n")
+    for item in question_open[key]:
+        write_image_opened.write(str(item) + "\n")
+    write_image_opened.write("\n")
+    write_image_opened.write("============================================================\n")
+
+print("question image  in opened" , len(question_open))
+
+
+print("question image  in closed" , len(question_closed))
+
 
 # head CT
 # head MRI

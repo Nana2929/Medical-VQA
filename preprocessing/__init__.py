991dd631713e3c0047e063cef713858c8fc9c0da
zhiao777774 <zhiaohsu@gmail.com>
2023-05-13 23:29:51 +0800

    update: change default pipeline for ABD

--- preprocessing/__init__.py
+++ preprocessing/__init__.py
@@ -18,7 +18,6 @@ _DEFAULT_PIPELINE_STEPS = {
     'ABD': [
         (remove_text, _MORPHOLOGY_KERNEL, _GAUSS_VALUE, (_CANNY_MIN, _CANNY_MAX)),
         (hu_transform, *_DEFAULT_HU_TRANSFORM_PARAMS['ABD']),
-        (adjust_tilt, 'ABD')
     ],
     'CHEST': [
         (remove_text, _MORPHOLOGY_KERNEL, _GAUSS_VALUE, (_CANNY_MIN, _CANNY_MAX)),

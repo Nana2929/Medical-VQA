a265aad2e3fe2fac57bbcf6bcef20ec7008aa0c9
Nana2929 <b06102020@ntu.edu.tw>
2023-05-14 17:17:47 +0800

    fix: channel bugs

--- preprocessing/_head_mri_preproc.py
+++ preprocessing/_head_mri_preproc.py
@@ -5,7 +5,7 @@ from skfuzzy import cmeans
 
 
 def _remove_noise(img):
-    img = img[:, :, 0]
+    # img = img[:, :, 0]
     mask = img <= 20
     selem = morphology.disk(2)
     segmentation = morphology.dilation(mask, selem)
@@ -26,34 +26,34 @@ def _fcm_class_mask(img, brain_mask):
     # We want 3 classes with a maximum of 50 iterations
     [class_centers, partitioned_matrix, _, _, _, _, _] = cmeans(img[brain_mask].reshape(-1, len(brain_mask[brain_mask])),
                                               3, 2, 0.005, 50)
-    
+
     # We use 'key' to sort by the values and not the index
     matrix_list = [partitioned_matrix[class_num] for class_num, _ in sorted(enumerate(class_centers),
-                                                                          key=lambda x: x[1])] 
+                                                                          key=lambda x: x[1])]
     mask = np.zeros(img.shape + (3,))
-    
+
     for index in range(3):
         mask[..., index][brain_mask] = matrix_list[index]
-    
+
     return mask
 
 
 def _get_mask(img):
-    mask = img == 0  
-    selem = morphology.disk(2)   
+    mask = img == 0
+    selem = morphology.disk(2)
     segmentation = morphology.dilation(mask, selem)
     labels, _ = ndimage.label(segmentation)
 
-    mask = labels == 0  
+    mask = labels == 0
     mask = morphology.dilation(mask, selem)
     mask = ndimage.binary_fill_holes(mask)
     mask = morphology.dilation(mask, selem)
-    
+
     # brain segmentation
     matrix_mask = _fcm_class_mask(img, mask)
     # white matter
     white_matter_mask = matrix_mask[..., 2] > 0.8
-    
+
     ''' If other masks are needed:
     # gray matter
     m1 = matrix_mask[..., 2] > 0.018
@@ -70,7 +70,7 @@ def _get_mask(img):
     m2 = matrix_mask[..., 2] > 0.8
     other_mask = m1 ^ m2
     '''
-    
+
     return white_matter_mask
 
 
@@ -78,7 +78,7 @@ def fcm_norm(img, norm_value):
     clean_img = _remove_noise(img)
     white_matter_mask = _get_mask(clean_img)
     wm_mean = clean_img[white_matter_mask == 1].mean()
-    
+
     normalized_img = (clean_img / wm_mean) * norm_value
-    
-    return normalized_img
+    # *= 255
+    return normalized_img*255

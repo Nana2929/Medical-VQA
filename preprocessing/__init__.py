7742e2cba744f162240dad59a8ed5ca4603889f2
Nana2929 <b06102020@ntu.edu.tw>
2023-05-14 18:20:58 +0800

    fix: efficiency

--- preprocessing/__init__.py
+++ preprocessing/__init__.py
@@ -2,6 +2,7 @@ import shutil
 import cv2
 import numpy as np
 from pathlib import Path
+from tqdm import tqdm
 
 from .config import (_DEFAULT_HU_TRANSFORM_PARAMS, _MORPHOLOGY_KERNEL,
                      _GAUSS_VALUE, _CANNY_MIN, _CANNY_MAX, _FCM_NORM_VALUE,
@@ -64,13 +65,16 @@ def pipeline(imgset, type: str, steps=None, out_dir=None):
     if out_dir:
         out_dir_path = f'{out_dir}/{type}'
         if Path(out_dir_path).exists():
-            shutil.rmtree(out_dir_path)
+            raise FileExistsError(
+                f'Output directory {out_dir_path} already exists.')
+            # shutil.rmtree(out_dir_path)
 
-    for img_name, img in imgset:
+
+
+    for img_name, img in tqdm(imgset):
         result = img.copy()
 
         # print(img_name, '\r\n')
-
         for i, step in enumerate(steps):
             method, *args = step
             # print(method.__name__, '\r\n')
@@ -85,6 +89,7 @@ def pipeline(imgset, type: str, steps=None, out_dir=None):
                     print(f'\tSkipped. Failed at {method.__name__} with {e}: the contour has no enough (5) points.')
             if out_dir:
                 out_path = f'{out_dir_path}/{i+1}_{method.__name__}'
+
                 Path(out_path).mkdir(parents=True, exist_ok=True)
 
                 cv2.imwrite(f'{out_path}/{img_name}', result)
